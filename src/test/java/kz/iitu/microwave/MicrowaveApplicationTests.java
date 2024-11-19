a9e95eaf1e4e528f5de5fedfcd771bbd224c9b4d
605-13 <eylshpack@gmail.com>
2024-11-19 12:11:33 +0500

    init commit

---
+++ src/test/java/kz/iitu/microwave/MicrowaveApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.microwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrowaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
